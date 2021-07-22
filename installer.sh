0373cc94377760262ed6c970e141cc99ac7a7e2e
Saurabh Datta <hi@dattasaurabh.com>
2021-07-22 17:45:34 +0800

    editing installer script - step 1-isntall and configure arduino cli

--- installer.sh
+++ installer.sh
@@ -3,16 +3,16 @@
 # Install arduino-cli, will install it in /home/pi/bin/ dir
 # curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
 
-cd \
+cd\
 mkdir /home/pi/bin
-echo 'export PATH=/home/pi/bin:$PATH' >> ~/.bashrc 
-source ~/.bashrc
+sudo echo 'export PATH=/home/pi/bin:$PATH' >> ~/.bashrc 
+sudo source home/pi/.bashrc
 cd /home/pi/bin
 wget https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Linux_ARMv7.tar.gz
 tar -xvzf arduino-cli_latest_Linux_ARMv7.tar.gz
 rm LICENSE.txt
 rm arduino-cli_latest_Linux_ARMv7.tar.gz
-cd \
+cd\
 
 
 
