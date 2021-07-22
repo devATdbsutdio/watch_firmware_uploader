c2114689cbba4b6383180f11e6bbcf43b84fa970
Saurabh Datta <hi@dattasaurabh.com>
2021-07-22 17:35:37 +0800

    first commit

---
+++ installer.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -i
+
+# Install arduino-cli, will install it in /home/pi/bin/ dir
+# curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
+
+cd \
+mkdir /home/pi/bin
+echo 'export PATH=/home/pi/bin:$PATH' >> ~/.bashrc 
+source ~/.bashrc
+cd /home/pi/bin
+wget https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Linux_ARMv7.tar.gz
+tar -xvzf arduino-cli_latest_Linux_ARMv7.tar.gz
+rm LICENSE.txt
+rm arduino-cli_latest_Linux_ARMv7.tar.gz
+cd \
+
+
+
+
