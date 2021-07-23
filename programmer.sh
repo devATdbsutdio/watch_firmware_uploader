892feb2d9f1e95ca4deb747c191a32e759d7777e
Saurabh Datta <hi@dattasaurabh.com>
2021-07-24 00:52:07 +0800

    working on uploading mechanism

--- programmer.sh
+++ programmer.sh
@@ -43,7 +43,7 @@ do
  select port in "${ports[@]}"; do
    FIRMWARE_REPO_DIR=$HOME/clock_firmware_production
    FIRMWARE_DIR=$FIRMWARE_REPO_DIR/clock
-   UPLOAD_CMD="$HOME/bin/arduino-cli compile -b megaTinyCore:megaavr:atxy7:chip=1607,clock=5internal,bodvoltage=1v8,bodmode=disabled,eesave=enable,millis=enabled,resetpin=UPDI,startuptime=0,uartvoltage=skip $FIRMWARE_DIR -u -p $port -P pyupdi -t"
+   UPLOAD_CMD=($HOME/bin/arduino-cli compile -b megaTinyCore:megaavr:atxy7:chip=1607,clock=5internal,bodvoltage=1v8,bodmode=disabled,eesave=enable,millis=enabled,resetpin=UPDI,startuptime=0,uartvoltage=skip $FIRMWARE_DIR -u -p $port -P pyupdi -t)
    #UPLOAD_CMD="$HOME/bin/arduino-cli compile --help"
    echo " " && echo " " && echo "Selected port is: [$REPLY] $port" && sleep 4 ; break
  done 
@@ -56,9 +56,7 @@ do
  echo "Uploading firmware now..."
  echo " "
  sleep 2
- echo "$UPLOAD_CMD"
- #$HOME/bin/arduino-cli compile --help
- $HOME/bin/arduino-cli compile -b megaTinyCore:megaavr:atxy7:chip=1607,clock=5internal,bodvoltage=1v8,bodmode=disabled,eesave=enable,millis=enabled,resetpin=UPDI,startuptime=0,uartvoltage=skip $FIRMWARE_DIR -u -p $port -P pyupdi -t
+ "${UPLOAD_CMD[@]}"
  cd $HOME
  sleep 10
  #clear
