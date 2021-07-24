8388fd4f14ca4c6bafb72f9a4f7aff57f9310573
Saurabh Datta <hi@dattasaurabh.com>
2021-07-24 22:49:29 +0800

    Testing footer in PI

--- test.sh
+++ test.sh
@@ -55,18 +55,19 @@ footer_status () {
 
 while true
 do
-  clear
-  set_window
-  footer_status
-  read -r -p "  > " input
-  case $input in
-    [pP])
-   footer_status
-   clear
-   ;;
-    *)
-   footer_status
-   clear
-   ;;
-   esac
+ clear
+ set_window
+ footer_status
+
+ read -r -p "  > " input
+ case $input in
+   [pP])
+ footer_status
+ clear
+ ;;
+  *)
+ footer_status
+ clear
+ ;;
+ esac
 done
