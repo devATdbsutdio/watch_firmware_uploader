176ca60636b48c135a49c32582b2ee93d58167dd
Saurabh Datta <hi@dattasaurabh.com>
2021-07-26 04:31:42 +0800

    adding configurator

--- installer.sh
+++ installer.sh
@@ -75,14 +75,14 @@ CLI_DOWNLOAD_LINK=""
 BIN_BASE_DIR=""
 CORE_URLS=""
 ARDUINO=""
-CONFIG_FILE=""
+CONFIG_FILE=$HOME/.arduino15/arduino-cli.yaml
 CORE=megaTinyCore
 CORE_COMB=megaTinyCore:megaavr
 LIB_LIST=(TinyMegaI2C RV8803Tiny)
 
 SETTING_FILE_NAME=settings.yaml
+
 # ymal_parse=$("which yq") #used for parsing setting file
-#
 ymal_parse=$HOME/bin/yq #used for parsing settings.yaml file
 
 FULL_PATH=$(realpath "$0")
@@ -98,22 +98,20 @@ clear
 sleep 1
 echo ""
 echo -e "${YELLOW} Loading settings ...${RESET}"
-sleep 4
+sleep 1
 if [ -f "$SETTINGS_FILE" ]; then
   echo -e "${GREEN}  TARGET SETTINGS EXIST IN: $SETTINGS_FILE${RESET}"
 
   CLI_DOWNLOAD_LINK=$($ymal_parse e '.BINARY.LINK' "$SETTINGS_FILE")
   BIN_BASE_DIR=$($ymal_parse e '.BINARY.BASE' "$SETTINGS_FILE")
   CORE_URLS=$($ymal_parse e '.BINARY.CORES.LINK[]' "$SETTINGS_FILE")
-  # CORE_URLS=(http://drazzy.com/package_drazzy.com_index.json)
   ARDUINO=""
-  CONFIG_FILE=$HOME/.arduino15/arduino-cli.yaml
 
   CORE=megaTinyCore
   CORE_COMB=megaTinyCore:megaavr
   LIB_LIST=(TinyMegaI2C RV8803Tiny)
 
-  sleep 3
+  sleep 4
   # Show the updated list and task to do
   settings_found_loaded=true
   process_list
@@ -128,84 +126,85 @@ else
 fi
 
 # -------------------- #
-CLI_DOWNLOAD_LINK=$($ymal_parse e '.BINARY.LINK' "$SETTINGS_FILE")
-BIN_BASE_DIR=$($ymal_parse e '.BINARY.BASE' "$SETTINGS_FILE")
-CORE_URL=http://drazzy.com/package_drazzy.com_index.json
-ARDUINO=""
-CONFIG_FILE=$HOME/.arduino15/arduino-cli.yaml
-
-CORE=megaTinyCore
-CORE_COMB=megaTinyCore:megaavr
-LIB_LIST=(TinyMegaI2C RV8803Tiny)
 
 # ---- Install arduino-cli ---- #
-# sleep 2
-# echo -e "${YELLOW}> Installing arduino-cli in target base directory:${RESET} $BIN_BASE_DIR"
-# echo ""
-# sleep 2
-# echo -e "${YELLOW}> Entering <base>/bin Directory:${RESET} cd $BIN_BASE_DIR/bin"
-# sleep 2
-# mkdir -p -- "$BIN_BASE_DIR"/bin
-# cd "$BIN_BASE_DIR"/bin || exit
-# echo -e "${GREEN}  IN $BIN_BASE_DIR/bin now${RESET}"
-# sleep 2
-# echo ""
-# echo -e "${YELLOW}> Downloading arduino-cli...${RESET}"
-# echo ""
-# sleep 2
-# wget "$CLI_DOWNLOAD_LINK"
-# echo -e "${GREEN}  Download finished!${RESET}"
-# sleep 2
-# echo ""
-# echo -e "${YELLOW}> Unzipping...${RESET}"
-# tar -xvzf arduino-cli_latest_Linux_ARMv7.tar.gz
-# rm arduino-cli_latest_Linux_ARMv7.tar.gz && rm LICENSE.txt
-# echo ""
-# echo -e "${GREEN}  arduino-cli installed in:${RESET} $BIN_BASE_DIR/bin/arduino-cli"
-# ARDUINO=$BIN_BASE_DIR/bin/arduino-cli
-# # ** Update cli's location in settings.yaml
-# echo -e "${GREEN}  Updated setting.yaml with arduino-cli's location${RESET}"
-# echo "" && echo ""
-# echo "---------------------------"
-# $ymal_parse e ".BINARY.LOCATION |= \"$ARDUINO\"" "$SETTINGS_FILE"
-# echo "---------------------------"
-# sleep 5
-# # go back to the home directory
-# cd "$HOME" || return
+sleep 2
+echo -e "${YELLOW}> Installing arduino-cli in target base directory:${RESET} $BIN_BASE_DIR"
+echo ""
+sleep 2
+echo -e "${YELLOW}> Entering <base>/bin Directory:${RESET} cd $BIN_BASE_DIR/bin"
+sleep 2
+mkdir -p -- "$BIN_BASE_DIR"/bin
+cd "$BIN_BASE_DIR"/bin || exit
+echo -e "${GREEN}  IN $BIN_BASE_DIR/bin now${RESET}"
+sleep 2
+echo ""
+echo -e "${YELLOW}> Downloading arduino-cli...${RESET}"
+echo ""
+sleep 2
+wget "$CLI_DOWNLOAD_LINK"
+echo -e "${GREEN}  Download finished!${RESET}"
+sleep 2
+echo ""
+echo -e "${YELLOW}> Unzipping...${RESET}"
+tar -xvzf arduino-cli_latest_Linux_ARMv7.tar.gz
+rm arduino-cli_latest_Linux_ARMv7.tar.gz && rm LICENSE.txt
+echo ""
+echo -e "${GREEN}  arduino-cli installed in:${RESET} $BIN_BASE_DIR/bin/arduino-cli"
+ARDUINO=$BIN_BASE_DIR/bin/arduino-cli
+# ** Update cli's location in settings.yaml
+echo -e "${GREEN}  Updated setting.yaml with arduino-cli's location${RESET}"
+echo "" && echo ""
+echo "---------------------------"
+$ymal_parse e ".BINARY.LOCATION |= \"$ARDUINO\"" "$SETTINGS_FILE"
+echo "---------------------------"
+sleep 4
+# go back to the home directory
+cd "$HOME" || return
 
 cli_installed=true
 process_list
 
 # ---- Create Arduino-cli init file [if it doesn't exist]---- #
-# echo -e "${YELLOW} Looking for arduino-cli config file...${RESET}"
-# if [ ! -f "$CONFIG_FILE" ]; then
-#   echo -e "${RED}  It doesn't exist!${RESET}"
-#   sleep 2
-#   echo -e "${YELLOW}  Creating now..${RESET}"
-#   echo ""
-#   "$ARDUINO" config init
-#   sleep 2
-#   echo ""
-#   echo "---------------------------"
-#   "$ARDUINO" config dump
-#   echo "---------------------------"
-# else
-#   echo -e "${GREEN}  It exists!${RESET}"
-#   sleep 2
-#   echo ""
-#   echo "---------------------------"
-#   "$ARDUINO" config dump
-#   echo "---------------------------"
-# fi
-# sleep 2
-
-cli_init_file_created=true
-process_list
+echo -e "${YELLOW}> Looking for arduino-cli config file...${RESET}"
+if [ ! -f "$CONFIG_FILE" ]; then
+  echo -e "${RED}  It doesn't exist!${RESET}"
+  sleep 2
+  echo -e "${YELLOW}  Creating now..${RESET}"
+  echo ""
+  "$ARDUINO" config init
+  sleep 2
+  echo ""
+  echo "---------------------------"
+  "$ARDUINO" config dump
+  echo "---------------------------"
+else
+  echo -e "${GREEN}  It exists!${RESET}"
+  sleep 2
+  echo ""
+  echo "---------------------------"
+  "$ARDUINO" config dump
+  echo "---------------------------"
+fi
 
 # ---- Add in board's manager additonal urls for MegaTinyCore ---- #
-for url in "${CORE_URLS[@]}"; do
-  echo "$url"
+for CORE_URL in "${CORE_URLS[@]}"; do
+  # echo "$CORE_URL"
+  ADD_CORE_URL="$ARDUINO config add board_manager.additional_urls $CORE_URL"
+  if grep -q "$CORE_URL" "$CONFIG_FILE"; then
+    echo "$CORE_URL already exists in config file"
+    sleep 2
+  else
+    echo "$CORE_URL doesn't exist in config file!"
+    sleep 2
+    echo "Adding $CORE_URL to config file"
+    sleep 2
+    $ADD_CORE_URL
+  fi
 done
+sleep 4
+cli_init_file_created=true
+process_list
 
 # ADD_CORE_URL="$ARDUINO config add board_manager.additional_urls $CORE_URL"
 
