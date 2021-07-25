c778efd24fa8c99649d1edf72a53801f67acafbd
Saurabh Datta <hi@dattasaurabh.com>
2021-07-25 19:47:59 +0800

    adding configurator

--- test.sh
+++ test.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # prints the name of the script
-echo $0
+#echo $0
 
 # prints the absolute path of the script 
 full_path=$(realpath $0)
-echo $full_path
+#echo $full_path
 
 # prints teh dir of the script
 SCRIPT_DIR=$(dirname $full_path)
-echo $SCRIPT_DIR
+#echo $SCRIPT_DIR
 
 $HOME/bin/yq e '.BINARY.LOCATION' $SCRIPT_DIR/settings.yaml 
