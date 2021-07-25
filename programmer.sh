6f7860f9fc47392d0937f722ffaad16278afd67b
Saurabh Datta <hi@dattasaurabh.com>
2021-07-25 17:37:39 +0800

    linting shell script

--- programmer.sh
+++ programmer.sh
@@ -92,7 +92,7 @@ do
   [sS])
  show_header_and_footer
 
- IFS=$'\n' ports=( $(ls /dev/tty*) )
+ IFS=$'\n' ports=( "$(ls /dev/tty*)" )
  select port in "${ports[@]}"; do
    FIRMWARE_REPO_DIR=$HOME/clock_firmware_production
    FIRMWARE_DIR=$FIRMWARE_REPO_DIR/clock
