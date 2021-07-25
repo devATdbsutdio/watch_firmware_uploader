694d162d49ff59a0e34fdb0a84683fa09aec7599
Saurabh Datta <hi@dattasaurabh.com>
2021-07-26 01:27:07 +0800

    adding configurator

--- installer.sh
+++ installer.sh
@@ -1,45 +1,36 @@
 #!/bin/bash
 
+SETTING_FILE_NAME=settings.yaml
+# ymal_parse=$("which yq") #used for parsing setting file
+ymal_parse=$HOME/bin/yq #used for parsing settings.yaml file
+
+FULL_PATH=$(realpath "$0")
+SETTINGS_DIR=$(dirname "$FULL_PATH")
+SETTINGS_FILE=$SETTINGS_DIR/$SETTING_FILE_NAME
+
+BIN_BASE_DIR=$($ymal_parse e '.BINARY.BASE' "$SETTINGS_FILE")
+CORE_URL=http://drazzy.com/package_drazzy.com_index.json
+ARDUINO=""
+CONFIG_FILE=$HOME/.arduino15/arduino-cli.yaml
+CORE=megaTinyCore
+CORE_COMB=megaTinyCore:megaavr
+LIB_LIST=(TinyMegaI2C RV8803Tiny)
+# TWOWIRELIB=TinyMegaI2C
+# RTCLIB=RV8803Tiny
+
 clear
 sleep 1
 
-# ---- SYS UPDATE & UPGRADE ---- #
-while true
-do
- read -r -p "Want to update and upgrade system? [Y/n] " input
- case $input in
-     [yY][eE][sS]|[yY])
- clear
- apt-get update -y
- apt-get upgrade -y
- clear
- echo "System Updated!"
- sleep 2
- clear	
- break
- ;;
-     [nN][oO]|[nN])
- clear
- echo "Moving on."
- break
-        ;;
-      *)
- echo "Invalid input..."
- ;;
- esac
-done
-
-
 # ---- Install arduino-cli ---- #
 sleep 1
-echo "Going to home directory..."
-cd $HOME
+echo "Going to base directory: $BIN_BASE_DIR"
+cd "$BIN_BASE_DIR" || return
 sleep 2
 echo "Making \"bin\" directory..."
 mkdir bin
 sleep 2
-echo "Going to $HOME/bin ..."
-cd $HOME/bin
+echo "Going to bin directory: $BIN_BASE_DIR/bin ..."
+cd "$BIN_BASE_DIR"/bin || return
 sleep 2
 echo "Downloading arduino-cli..."
 sleep 2
@@ -50,24 +41,26 @@ echo "Unzipping..."
 tar -xvzf arduino-cli_latest_Linux_ARMv7.tar.gz
 rm arduino-cli_latest_Linux_ARMv7.tar.gz
 rm LICENSE.txt
-echo "arduino-cli installed in $HOME/bin/arduino-cli"
-sleep 4
-cd $HOME/clock_uploader_machine
+echo "arduino-cli installed in $BIN_BASE_DIR/bin/arduino-cli"
+ARDUINO=$BIN_BASE_DIR/bin/arduino-cli
+# ** Entry cli's location in settings.yaml
+$ymal_parse e ".BINARY.LOCATION |= $ARDUINO" "$SETTINGS_FILE"
 
+sleep 4
+# go back to the home directory
+cd "$HOME" || return
 
 # ---- Create Arduino-cli init file [if it doesn't exist]---- #
-CONFIG_FILE=$HOME/.arduino15/arduino-cli.yaml
-[ ! -f $CONFIG_FILE ] && $HOME/bin/arduino-cli config init && echo "There is Config file now!"
+[ ! -f "$CONFIG_FILE" ] && "$ARDUINO" config init && echo "There is Config file now!"
 
 sleep 2
 
 # ---- Add in board's manager additonal urls for MegaTinyCore ---- #
-CORE_URL=http://drazzy.com/package_drazzy.com_index.json
-ADD_CORE_URL="$HOME/bin/arduino-cli config add board_manager.additional_urls $CORE_URL"
+ADD_CORE_URL="$ARDUINO config add board_manager.additional_urls $CORE_URL"
 
-if grep -q "$CORE_URL" "$CONFIG_FILE" ; then
+if grep -q "$CORE_URL" "$CONFIG_FILE"; then
   echo "$CORE_URL already exists in config file"
-  sleep 2 
+  sleep 2
 else
   echo "$CORE_URL doesn't exist in config file!"
   sleep 2
@@ -76,64 +69,54 @@ else
   $ADD_CORE_URL
 fi
 
-
 # ---- Install the megaTinyCore ---- #
-CORE=megaTinyCore
-CORE_EXT=megaavr
-CORE_COMB=megaTinyCore:megaavr
-SEARCH_CMD="$HOME/bin/arduino-cli core search $CORE"
-CORE_INSTALL_CMD="$HOME/bin/arduino-cli core install $CORE_COMB"
+SEARCH_CMD="$ARDUINO core search $CORE"
+CORE_INSTALL_CMD="$ARDUINO core install $CORE_COMB"
 
 echo "Searching $CORE..."
 
-if ! "$($SEARCH_CMD)" =~ "No" ; then
+if [[ ! "$($SEARCH_CMD)" =~ "No" ]]; then
   echo "Core found. Installing now ..."
   sleep 2
   $CORE_INSTALL_CMD
-elif "$($SEARCH_CMD)" =~ "No" ; then
+elif [[ "$($SEARCH_CMD)" =~ "No" ]]; then
   echo "No such Core !"
   sleep 2
 fi
 
-# [[ ! "$($SEARCH_CMD)" =~ "No" ]] && $CORE_INSTALL_CMD
-
 # ---- Install the necessary libraries  ---- #
-TWOWIRELIB=TinyMegaI2C
-RTCLIB=RV8803Tiny
-LIBSEARCH_CMD="$HOME/bin/arduino-cli lib search"
-LIBLIST_CMD="$HOME/bin/arduino-cli lib list"
-LIBINSTALL_CMD="$HOME/bin/arduino-cli lib install"
+LIBSEARCH_CMD="$ARDUINO lib search"
+LIBINSTALL_CMD="$ARDUINO lib install"
 
-echo "Searching $TWOWIRELIB in Library manager..."
-if [[ "$($LIBSEARCH_CMD $TWOWIRELIB --names)" == *$TWOWIRELIB* ]]; then
- echo "library found in Library Manager Repo:)"
- echo "Installing $TWOWIRELIB Library..."
- $LIBINSTALL_CMD $TWOWIRELIB
-else
- echo "$TWOWIRELIB library not found!"
-fi
+for LIB in "${LIB_LIST[@]}"; do
+  echo "Searching $LIB in Library manager..."
+  if [[ "$($LIBSEARCH_CMD "$LIB" --names)" == *$LIB* ]]; then
+    echo "Library found in Library Manager Repo!)"
+    sleep 2
+    $LIBINSTALL_CMD "$LIB"
+  else
+    echo "$LIB library not found!"
+  fi
+done
 
-echo "Searching $RTCLIB in Library manager..."
-if [[ "$($LIBSEARCH_CMD $RTCLIB --names)" == *$RTCLIB* ]]; then
- echo "library found in Library Manager Repo :)"
- echo "Installing $RTCLIB Library..."
- $LIBINSTALL_CMD $RTCLIB
-else
- echo "$RTCLILB library not found!"
-fi
+# echo "Searching $TWOWIRELIB in Library manager..."
+# if [[ "$($LIBSEARCH_CMD $TWOWIRELIB --names)" == *$TWOWIRELIB* ]]; then
+#   echo "library found in Library Manager Repo:)"
+#   echo "Installing $TWOWIRELIB Library..."
+#   $LIBINSTALL_CMD $TWOWIRELIB
+# else
+#   echo "$TWOWIRELIB library not found!"
+# fi
 
+# echo "Searching $RTCLIB in Library manager..."
+# if [[ "$($LIBSEARCH_CMD $RTCLIB --names)" == *$RTCLIB* ]]; then
+#   echo "library found in Library Manager Repo :)"
+#   echo "Installing $RTCLIB Library..."
+#   $LIBINSTALL_CMD $RTCLIB
+# else
+#   echo "$RTCLIB library not found!"
+# fi
 
 # ---- git clone the firmware source code ---- #
-cd $HOME
-git clone https://github.com/dattasaurabh82/clock_firmware_production.git
-
-
-# ---- auto load programmer.sh ---- #
-# TBD
-
-
-
-
-
-
-
+# cd "$HOME" || return
+# git clone https://github.com/dattasaurabh82/clock_firmware_production.git
