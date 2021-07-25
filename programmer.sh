ad7ffbfedc913a16d74675f975bcdaad89cb101f
Saurabh Datta <hi@dattasaurabh.com>
2021-07-25 21:34:21 +0800

    adding configurator

--- programmer.sh
+++ programmer.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-
 FULL_PATH=$(realpath "$0")
 SETTINGS_DIR=$(dirname "$FULL_PATH")
 SETTINGS_FILE=$SETTINGS_DIR/settings.yaml
-
 
 FIRMWARE_REPO_DIR=$HOME/clock_firmware_production
 FIRMWARE_DIR=$FIRMWARE_REPO_DIR/clock
@@ -25,16 +23,11 @@ PORT="--"
 PROGRAMMER=$("$HOME"/bin/yq e '.MICROCONTROLLER.PROGRAMMER' "$SETTINGS_FILE")
 FIRMWARE_DIR=$("$HOME"/bin/yq e '.FIRMWARE.DIR' "$SETTINGS_FILE")
 
-
 UPLOAD_CMD=("$BIN" compile -b "$CORE":chip="$CHIP",clock="$CLOCK",bodvoltage="$BOD",bodmode="$BODMODE",eesave="$EEPROM_SAVE",millis="$MILLIS",resetpin="$RESET_PIN",startuptime="$STARTUP_TIME",uartvoltage="$UARTV" "$FIRMWARE_DIR" -u -p "$PORT" -P "$PROGRAMMER" -t)
 
 echo "${UPLOAD_CMD[@]}"
 
-
 #SER_PORT_REGEX='/dev/ttyA\|/dev/ttyp\|/dev/ttyU\|/dev/tty.u'
-
-
-
 
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
@@ -44,7 +37,6 @@ WHOLE_LINE_YELLOW='\x1b[43;30m'
 WHOLE_LINE_RESET='\x1b[K\x1b[0m'
 
 HEIGHT=$(tput lines)
-
 
 BANNER="
    __ _                                       
@@ -59,8 +51,6 @@ BANNER="
        |_|
 "
 
-
-
 PORT_STAT="PORT: "$PORT""
 LAST_PULL=$(<"$last_pull_info_file")
 PULL_STAT="LAST PULL: "$LAST_PULL""
@@ -68,24 +58,27 @@ LAST_BURN="--"
 BURN_STAT="FIRMWARE BURN STAT: "$LAST_BURN""
 BOTT_STAT=""$PORT_STAT" | "$PULL_STAT" | "$BURN_STAT""
 
-
-
-set_window (){
-    # Create a virtual window that is two lines smaller at the bottom.
-    tput csr 0 $(( HEIGHT-2 ))
+set_window() {
+  # Create a virtual window that is two lines smaller at the bottom.
+  tput csr 0 $((HEIGHT - 2))
 }
 
-show_header_and_footer (){
-  clear 
+show_header_and_footer() {
+  clear
 
   echo -e "${YELLOW}$BANNER${RESET}" && echo " " && echo " "
   echo -e "${YELLOW} PRESS [ P ] THEN [ ENTER ] -> GET LATEST FIRMWARE."
   echo -e "${YELLOW} PRESS [ S ] THEN [ ENTER ] -> SELECT UPLOAD PORT [ YOUR DEVICE SHOULD BE C ONNECTED FOR THIS STEP ]."
   echo -e "${YELLOW} PRESS [ U ] THEN [ ENTER ] -> UPLOAD FIRMWARE [ YOUR DEVICE SHOULD BE CONNECTED FOR THIS STEP ].${RESET}" && echo " "
 
- # Move cursor to last line in your screen
-  tput cup "$HEIGHT" 0;
+  # Move cursor to last line in your screen
+  tput cup "$HEIGHT" 0
 
+  PORT_STAT="$PORT_STAT"
+  LAST_PULL="$LAST_PULL"
+  PULL_STAT="LAST PULL: "$LAST_PULL""
+  LAST_BURN="$LAST_BURN"
+  BURN_STAT="FIRMWARE BURN STAT: "$LAST_BURN""
   BOTT_STAT=""$PORT_STAT" | "$PULL_STAT" | "$BURN_STAT""
   echo -e ""${WHOLE_LINE_YELLOW}""$BOTT_STAT""${WHOLE_LINE_RESET}""
 
@@ -93,61 +86,61 @@ show_header_and_footer (){
   tput cup 17 0
 }
 
+while true; do
+  clear
+  set_window
+  show_header_and_footer
 
-while true
-do
- clear
- set_window
- show_header_and_footer
+  read -r -p "  > " input
+  case $input in
+  [pP])
+    # -------- CURR CHECK AREA -------
+    LAST_PULL="pulling now..."
+    show_header_and_footer
 
- read -r -p "  > " input
- case $input in
-   [pP])
- LAST_PULL="pulling now..."
- show_header_and_footer
- # -------- CURR CHECK AREA ------- 
- echo ""$FIRMWARE_REPO_DIR" && git pull"
- cd "$FIRMWARE_REPO_DIR" && git pull 
- cd "$HOME"
- sleep 2 
- # -------- CURR CHECK AREA ------- 
- # get current time stamp
- current_date_time="$(date +"%Y-%m-%d %T")"
- # show curr time stamp
- LAST_PULL="$current_date_time"
- # save curr time stamp
- echo "$LAST_PULL" > "$last_pull_info_file"
- sleep 5
- clear
- ;;
+    echo ""$FIRMWARE_REPO_DIR" && git pull"
+    cd "$FIRMWARE_REPO_DIR" && git pull
+    cd "$HOME"
+    sleep 2
+    # -------- CURR CHECK AREA -------
+    # get current time stamp
+    current_date_time="$(date +"%Y-%m-%d %T")"
+    # show curr time stamp
+    LAST_PULL="$current_date_time"
+    # save curr time stamp
+    echo "$LAST_PULL" >"$last_pull_info_file"
+    sleep 5
+    clear
+    ;;
   [sS])
- show_header_and_footer
+    show_header_and_footer
 
- IFS=$'\n' ports=( $(find /dev/tty*) )
- select port in "${ports[@]}"; do
-   PORT="$port"
-   PORT_STAT="PORT: $PORT" ; break
- done
- clear
- ;;
-   [uU])
- LAST_BURN="Uploading now..."
- show_header_and_footer
+    IFS=$'\n' ports=($(find /dev/tty*))
+    select port in "${ports[@]}"; do
+      PORT="$port"
+      PORT_STAT="PORT: $PORT"
+      break
+    done
+    clear
+    ;;
+  [uU])
+    LAST_BURN="Uploading now..."
+    show_header_and_footer
 
- echo "${UPLOAD_CMD[@]}" 
- ${UPLOAD_CMD[@]}
- 
- burn_date_time="$(date +"%Y-%m-%d %T")"
- LAST_BURN="Last burnt at $burn_date_time"
- show_header_and_footer
- sleep 2
- clear 
- ;;
+    echo "${UPLOAD_CMD[@]}"
+    ${UPLOAD_CMD[@]}
+
+    burn_date_time="$(date +"%Y-%m-%d %T")"
+    LAST_BURN="Last burnt at $burn_date_time"
+    show_header_and_footer
+    sleep 2
+    clear
+    ;;
   *)
- # -- [TBD] show invalid input status
- show_header_and_footer
- sleep 2
- clear
- ;;
- esac
-done 
+    # -- [TBD] show invalid input status
+    show_header_and_footer
+    sleep 2
+    clear
+    ;;
+  esac
+done
