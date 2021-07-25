70ff44c708c5e9d6ad3d87b7c9ae6ef77cf7bbcd
Saurabh Datta <hi@dattasaurabh.com>
2021-07-26 01:56:11 +0800

    adding configurator

--- installer.sh
+++ installer.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
 
+# ---- color info ---- #
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+RED='\033[0;31m'
+RESET='\033[0m'
+# -------------------- #
+
+# ---- Pre-checks ---- #
+clear
+sleep 1
+echo ""
+echo -e "${YELLOW}Loading settings ...${RESET}"
+sleep 4
+if [ -f "$SETTINGS_FILE" ]; then
+  echo -e "${GREEN}TARGET SETTINGS EXIST IN: $SETTINGS_FILE${RESET}"
+  sleep 5
+else
+  echo -e "${RED}TARGET SETTINGS file $SETTING_FILE_NAME doesn't seem to exist in: $SETTINGS_DIR/"
+  echo -e "QUITTING !${RESET}"
+  sleep 5
+  exit 1
+fi
+# -------------------- #
+
 SETTING_FILE_NAME=settings.yaml
 # ymal_parse=$("which yq") #used for parsing setting file
+#
 ymal_parse=$HOME/bin/yq #used for parsing settings.yaml file
 
 FULL_PATH=$(realpath "$0")
@@ -12,19 +37,22 @@ BIN_BASE_DIR=$($ymal_parse e '.BINARY.BASE' "$SETTINGS_FILE")
 CORE_URL=http://drazzy.com/package_drazzy.com_index.json
 ARDUINO=""
 CONFIG_FILE=$HOME/.arduino15/arduino-cli.yaml
+
 CORE=megaTinyCore
 CORE_COMB=megaTinyCore:megaavr
 LIB_LIST=(TinyMegaI2C RV8803Tiny)
-# TWOWIRELIB=TinyMegaI2C
-# RTCLIB=RV8803Tiny
-
-clear
-sleep 1
 
 # ---- Install arduino-cli ---- #
 sleep 1
+echo ""
 echo "Going to base directory: $BIN_BASE_DIR"
-cd "$BIN_BASE_DIR" || return
+if [[ ! "$(cd "$BIN_BASE_DIR")" ]]; then
+  echo "$BIN_BASE_DIR Doesn't exist. Creating now ..."
+  sleep 2
+  mkdir "$BIN_BASE_DIR"
+  cd "$BIN_BASE_DIR" || return
+fi
+# cd "$BIN_BASE_DIR" || return
 sleep 2
 echo "Making \"bin\" directory..."
 mkdir bin
@@ -45,14 +73,13 @@ echo "arduino-cli installed in $BIN_BASE_DIR/bin/arduino-cli"
 ARDUINO=$BIN_BASE_DIR/bin/arduino-cli
 # ** Entry cli's location in settings.yaml
 $ymal_parse e ".BINARY.LOCATION |= \"$ARDUINO\"" "$SETTINGS_FILE"
-
-sleep 4
+sleep 2
 # go back to the home directory
 cd "$HOME" || return
 
 # ---- Create Arduino-cli init file [if it doesn't exist]---- #
+echo ""
 [ ! -f "$CONFIG_FILE" ] && "$ARDUINO" config init && echo "There is Config file now!"
-
 sleep 2
 
 # ---- Add in board's manager additonal urls for MegaTinyCore ---- #
@@ -73,14 +100,17 @@ fi
 SEARCH_CMD="$ARDUINO core search $CORE"
 CORE_INSTALL_CMD="$ARDUINO core install $CORE_COMB"
 
+echo ""
 echo "Searching $CORE..."
 
 if [[ ! "$($SEARCH_CMD)" =~ "No" ]]; then
   echo "Core found. Installing now ..."
   sleep 2
   $CORE_INSTALL_CMD
+  echo ""
 elif [[ "$($SEARCH_CMD)" =~ "No" ]]; then
   echo "No such Core !"
+  echo ""
   sleep 2
 fi
 
@@ -98,24 +128,6 @@ for LIB in "${LIB_LIST[@]}"; do
     echo "$LIB library not found!"
   fi
 done
-
-# echo "Searching $TWOWIRELIB in Library manager..."
-# if [[ "$($LIBSEARCH_CMD $TWOWIRELIB --names)" == *$TWOWIRELIB* ]]; then
-#   echo "library found in Library Manager Repo:)"
-#   echo "Installing $TWOWIRELIB Library..."
-#   $LIBINSTALL_CMD $TWOWIRELIB
-# else
-#   echo "$TWOWIRELIB library not found!"
-# fi
-
-# echo "Searching $RTCLIB in Library manager..."
-# if [[ "$($LIBSEARCH_CMD $RTCLIB --names)" == *$RTCLIB* ]]; then
-#   echo "library found in Library Manager Repo :)"
-#   echo "Installing $RTCLIB Library..."
-#   $LIBINSTALL_CMD $RTCLIB
-# else
-#   echo "$RTCLIB library not found!"
-# fi
 
 # ---- git clone the firmware source code ---- #
 # cd "$HOME" || return
