4281a777de8a2f365c7b04ae3f846e314ce0748b
Saurabh Datta <hi@dattasaurabh.com>
2021-07-25 19:31:41 +0800

    adding configurator

--- test.sh
+++ test.sh
@@ -1,76 +1,9 @@
 #!/bin/bash
 
-
-#TWOWIRELIB=TinyMegaI2C
-#RTCLIB=RV8803Tiny
-#LIBSEARCH_CMD="arduino-cli lib search"
-
-#$LIBSEARCH_CMD $TWOWIRELIB --names
-
-#if [[ "$($LIBSEARCH_CMD $TWOWIRELIB --names)" == *$TWOWIRELIB* ]]; then
-#  echo "library found"
-#else
-#  echo "library not found"
-#fi
-
-#countdown() {
-#        msg="BACK TO PROMPT IN: "
-#        clear
-#        tput cup 10 5
-#        echo -n "$msg"
-#        l=${#msg}
-#        l=$(( l+5 ))
-#        for i in {2..1}
-#        do
-#                tput cup 10 $l
-#                echo -n "$i"
-#                sleep 1
-#        done
-#}
-
-#countdown
-
-#UPLOAD_CMD=($HOME/bin/arduino-cli compile -b megaTinyCore:megaavr:atxy7:chip=1607,clock=5internal,bodvoltage=1v8,bodmode=disabled,eesave=enable,millis=enabled,resetpin=UPDI,startuptime=0,uartvoltage=skip $FIRMWARE_DIR -u -p $port -P pyupdi -t)
-
-
-WHOLE_LINE_YELLOW='\x1b[43;30m'
-#WHOLE_LINE_YELLOW='\x1b[43;30m\n\x1b[0m\x1b[43;30m'
-WHOLE_LINE_RESET='\x1b[K\x1b[0m'
-YELLOW='\033[0;33m'
-RESET='\033[0m'
-
-HEIGHT=$(tput lines)
-
-set_window (){
-  # Create a virtual window that is two lines smaller at the bottom.
-  tput csr 0 $(($HEIGHT-2))
-}
-
-footer_status () {
-  # Move cursor to last line in your screen
-  tput cup $HEIGHT 0;
-  
-  echo -e "${WHOLE_LINE_YELLOW}TEST STATUS STRING${WHOLE_LINE_RESET}"
-
-  # Move cursor to home position, back in virtual window
-  tput cup 0 0
-}
-
-while true
-do
- clear
- set_window
- footer_status
-
- read -r -p "  > " input
- case $input in
-   [pP])
- footer_status
- clear
- ;;
-  *)
- footer_status
- clear
- ;;
- esac
-done
+echo $0
+ 
+full_path=$(realpath $0)
+echo $full_path
+ 
+dir_path=$(dirname $full_path)
+echo $dir_path
