2a0765eee9dffb3e68a6007da9c1eb235361e5b5
Saurabh Datta <hi@dattasaurabh.com>
2021-07-26 05:14:00 +0800

    adding configurator

--- installer.sh
+++ installer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # # ---- color info ---- #
-# GREEN='\033[0;32m'
-# YELLOW='\033[0;33m'
-# RED='\033[0;31m'
-# RESET='\033[0m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+RED='\033[0;31m'
+RESET='\033[0m'
 # # -------------------- #
 
 # settings_found_loaded=false
