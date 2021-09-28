bf93dd2d81224653f657273a3c09586fe960ef4c
Saurabh Datta <hi@dattasaurabh.com>
2021-09-28 21:29:30 +0800

    adding test code and new UI

--- keyboard.py
+++ keyboard.py
@@ -1,49 +1,8 @@
-# from pynput import keyboard
+import threading
 
-# port_selection_active = False
+port_selection_active = False
 # spl_key = False
-# # pull 
 
-# def on_press(key):
-# 	global port_selection_active
-# 	global spl_key
-# 	try:
-# 		spl_key = False
-# 		if key.char == 's':
-# 			port_selection_active = not port_selection_active
-# 			# print(port_selection_active)
-# 		# if key.char == 'p':
-# 			# port_selection_active = not port_selection_active
-# 			# print(port_selection_active)
-# 	except AttributeError:
-# 		spl_key = True
-
-# # ...or, in a non-blocking fashion:
-# listener = keyboard.Listener(on_press=on_press)
-
-# listener.start()
-
-# # while True:
-# # 	t = 1
-
-
-# from key_getter import KeyGetter
-# import threading
-
-# def kbd_watcher(): # Test with block=False
-#     k = KeyGetter()
-#     try:
-#         while True:
-#             if k.kbhit():
-#             	s=1
-#                 # print('Got', repr(k.getch(False)))
-#     except KeyboardInterrupt:
-#         pass
-
-# # runKBDwatcher()
-
-# kbd_watch_routine = threading.Thread(target=kbd_watcher())
-# kbd_watch_routine.start()
 
 import sys
 import tty
@@ -54,33 +13,39 @@ EOT = '\x04'  # CTRL+D
 ESC = '\x1b'
 CSI = '['
 
-line = ''
+string = ''
 
 def getchar():
-    fd = sys.stdin.fileno()
-    attr = termios.tcgetattr(fd)
-    try:
-        tty.setraw(fd)
-        return sys.stdin.read(1)
-    finally:
-        termios.tcsetattr(fd, termios.TCSANOW, attr)
+	fd = sys.stdin.fileno()
+	attr = termios.tcgetattr(fd)
+	try:
+		tty.setraw(fd)
+		return sys.stdin.read(1)
+	finally:
+		termios.tcsetattr(fd, termios.TCSANOW, attr)
 
-while True:
-    c = getchar()
-    if c == EOT or c == EOC:
-        # print('exit')
-        break
-    elif c == ESC:
-        if getchar() == CSI:
-            x = getchar()
-            if x == 'A':
-                print('UP')
-            elif x == 'B':
-                print('DOWN')
-    elif c == '\r':
-    	# print('>' + c)
-        print([line])
-        line = ''
-        # c = ''
-    else:
-        line += c
+
+def watch_kbd():
+	string=''
+	global port_selection_active
+
+	while True:
+		c = getchar()
+
+		if c == EOT or c == EOC or c == ESC:
+			break
+		elif c == '\r':
+			print(string)
+
+			if string == 's':
+				port_selection_active
+				port_selection_active = not port_selection_active
+				# print(port_selection_active)
+
+			string = ''
+		else:
+			string += c
+
+
+kbd_thread = threading.Thread(target=watch_kbd)
+kbd_thread.start()
