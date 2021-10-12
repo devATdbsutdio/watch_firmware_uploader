ff876ab3ef4c0b9513860927b8428f045f9d10c3
Saurabh Datta <hi@dattasaurabh.com>
2021-10-13 03:02:02 +0800

    adding git up in proper dir

--- keyboard.py
+++ keyboard.py
@@ -92,9 +92,9 @@ def watch_kbd():
 			elif string == 'p':
 				#--- Pull latest firmware ---#
 				# action.execute(["cmd_arg", "cmd_arg", ...], <timeout_value_in_sec>)
-				# action.execute(gv.git_pull_cmd, 120)
-				logger.log_info(gv.git_pull_cmd)
-				gv.output_msg_buff = [gv.git_pull_cmd]
+				logger.log_info(''.join(gv.git_pull_cmd))
+				gv.output_msg_buff = [''.join(gv.git_pull_cmd)]
+				action.execute(gv.git_pull_cmd, 120)
 			elif string == 'u':
 				#--- Upload current firmware (which ever it is (prod or test))
 				#  In UI, show that the "upload firmware" cmd will be executed
