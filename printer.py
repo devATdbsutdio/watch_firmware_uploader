530fa3897bf32d9ff4f156441c004c8cd3fb7e73
Saurabh Datta <hi@dattasaurabh.com>
2021-10-12 18:50:15 +0800

    Adding thermal printer capabilities and linting

--- printer.py
+++ printer.py
@@ -25,23 +25,19 @@ def have_printer_port(_filter_port):
 	state = False
 
 	if sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-		if _filter_port in raw_ports:
-			state = True
-		else:
-			state = False
+		state = bool(_filter_port in raw_ports)
+
 	if sys.platform.startswith('darwin'):
-		if _filter_port in raw_ports:
-			state = True
-		else:
-			state = False
+		state = bool(_filter_port in raw_ports)
 	return state
 
 
 
 
 def print_text(_filter_port, _data):
+	'''Thermal printer pritning logic and utility function'''
 	if have_printer_port(_filter_port):
-		printer = ThermalPrinter(port=_filter_port, baudrate = 19200)
+		printer = ThermalPrinter(port=_filter_port, baudrate=19200)
 		have_paper = False
 
 		if printer.is_online is False:
@@ -49,25 +45,19 @@ def print_text(_filter_port, _data):
 		if printer.is_sleeping:
 			printer.wake()
 
-		if printer.status()['paper']:
-			have_paper = True
-		else:
-			have_paper = False
+		have_paper = bool(printer.status()['paper'])
 
 		if have_paper:
 			if isinstance(_data, str):
 				if _data.startswith(gv.heading_identifier): # it is a heading
 					# get rid of the heading identifier from teh serial string
 					_data = _data.replace(gv.heading_identifier, '')
-					printer.out(_data, bold=True, justify='L',left_margin=0, size='S')
+					printer.out(_data, bold=True, justify='L', left_margin=0, size='S')
 				elif _data.startswith('TEST'): # it is a header or a footer
 					printer.out(_data, bold=True, justify='L', left_margin=0, size='S', underline=1)
 					# printer.out(_item, underline=1)
-				elif _data.startswith('CHECKING'): # it is a header or a footer
-						s=1
-						# Do not print anything
 				else:
-					printer.out(_data, bold=False, justify='L',left_margin=0, size='S')
+					printer.out(_data, bold=False, justify='L', left_margin=0, size='S')
 			if isinstance(_data, list):
 				for _item in _data:
 					if _item.startswith(gv.heading_identifier): # it is a heading
@@ -76,27 +66,24 @@ def print_text(_filter_port, _data):
 					elif _item.startswith('TEST'): # it is a header or a footer
 						printer.out(_item, bold=True, justify='L', left_margin=0, size='S', underline=1)
 						# printer.out(_item, underline=1)
-					elif _item.startswith('CHECKING'): # it is a header or a footer
-						s=1
-						# Do not print anything
 					else:
-						printer.out(_item, bold=False, justify='L',left_margin=0, size='S')
+						printer.out(_item, bold=False, justify='L', left_margin=0, size='S')
 			printer.feed(4)
 		else:
 			# print("No paper")
-			logger.log_warning(["", "You asked a Thermal printer to print the log.", 
-								"But there is no paper in the printer"
-						  	])
+			logger.log_warning(["", "You asked a Thermal printer to print the log.",
+								               "But there is no paper in the printer"
+						  	             ])
 			gv.output_msg_buff = ["", "Asked for physical log.", "But no paper in printer!"]
 	else:
 		# print("No printer")
-		logger.log_warning(["", "You asked a Thermal printer to print the log.", 
-							"But there is no Thermal printer on the port:",
-							gv.printer_port
-						  ])
-		gv.output_msg_buff = ["", "Asked for physical log.", "But no printer on the port:", 
-							  gv.printer_port
-							 ]
+		logger.log_warning(["", "You asked a Thermal printer to print the log.",
+							               "But there is no Thermal printer on the port:",
+							               gv.printer_port
+						               ])
+		gv.output_msg_buff = ["", "Asked for physical log.", "But no printer on the port:",
+							                 gv.printer_port
+							                ]
 
 
 # print_text('/dev/tty.usbserial-AI05HDSG', '[H]printer is there!')
