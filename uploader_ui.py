d0e98ad7f9942a5869545977d950d4e8fc0db4dd
Saurabh Datta <hi@dattasaurabh.com>
2021-10-19 00:05:20 +0800

    editing for local pi

--- uploader_ui.py
+++ uploader_ui.py
@@ -117,7 +117,7 @@ class App(npyscreen.NPSApp):
             BufferPagerBox,
             name='PROCESS OUTPUT MONITOR',
             rely=output_pos_y,
-            height=18,
+            height=19,
             editable=False,
             color='WARNING'
         )
