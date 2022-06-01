f863bdcb5ca231f7768e5ee9197aac4c49f328ff
Saurabh Datta <hi@dattasaurabh.com>
2022-06-01 15:44:39 +0800

    fiox test lim import placement

--- pythonBased/serialport_manager.py
+++ pythonBased/serialport_manager.py
@@ -15,7 +15,7 @@ import time
 import sys
 import serial
 import serial.tools.list_ports
-from ftfy import fix_text
+# from ftfy import fix_text
 import global_vars as gv
 import logger
 
@@ -109,7 +109,7 @@ def get_ser_data_line():
 					incoming_line = (incoming_line.decode()+temp.decode()).encode()
 
 			# special function to handle special characters if serial spits out garbage
-			incoming_line = fix_text(incoming_line) 
+			# incoming_line = fix_text(incoming_line) 
 			incoming_line = incoming_line.decode()
 			incoming_line = incoming_line.strip()
 
